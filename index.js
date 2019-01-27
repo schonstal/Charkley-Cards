ab77106d410bcf348eabe9e5c8b77949fde25da1
Josh Schonstal <josh@schonstal.org>
2019-01-26 23:04:31 -0800

    🔧 Lots of stuff (game jam)

--- index.js
+++ index.js
@@ -14,28 +14,30 @@ io.on('connection', function(socket){
   let _channel = null;
   let _username = null;
 
-  socket.on('add_name', function({ username, name }) {
+  socket.on('add_name', function(name) {
     channels[_channel].users[_username].addName(name);
 
-    console.log(`${username} submitted name ${name}`);
+    console.log(`[${new Date()} ${_username} submitted name ${name} to ${_channel}`);
   });
 
-  socket.on('add_flavor', function({ username, flavor }) {
+  socket.on('add_flavor', function(flavor) {
     channels[_channel].users[_username].addFlavor(flavor);
 
-    console.log(`${username} submitted flavor ${flavor}`);
+    console.log(`[${new Date()}] ${_username} submitted flavor ${flavor} to ${_channel}`);
   });
 
   socket.on('join', function({ username, channel }) {
-    console.log(`got a join for ${channel} by ${username}`);
+    console.log(`[${new Date()}] ${username} joined game: ${channel}`);
 
     if (channels[channel] === undefined) {
-      channels[channel] = new Channel();
+      channels[channel] = new Channel(channel, 10000);
     }
 
     _channel = channel;
     _username = username;
-    channels[channel].addUser(username);
+    channels[channel].addUser(username, socket);
+
+    socket.emit('change_phase', channels[channel].currentPhase);
   });
 });
 
@@ -44,22 +46,42 @@ http.listen(port, function(){
 });
 
 class Channel {
-  constructor() {
+  constructor(name, gameTime = 30000) {
     this.users = {};
+    this.currentPhase = 'add-name';
+    this.name = name;
+
+    setTimeout(() => {
+      this.switchPhase('add-flavor');
+      setTimeout(() => {
+        this.switchPhase('make-cards');
+      }, gameTime);
+    }, gameTime);
   }
 
-  addUser(username) {
-    users[username] = new User(username);
+  addUser(username, socket) {
+    this.users[username] = new User(username, socket);
+  }
+
+  switchPhase(phase) {
+    console.log(`[${new Date()}] game ${this.name} switching to phase: ${phase}`)
+    this.currentPhase = phase;
+
+    let entries = Object.entries(this.users);
+    for (const [username, user] of entries) {
+      user.socket.emit('change_phase', phase);
+    }
   }
 }
 
 class User {
-  constructor(username) {
+  constructor(username, socket) {
     this.username = username;
 
     this.score = 0;
     this.nameSubmissions = [];
     this.flavorSubmissions = [];
+    this.socket = socket;
   }
 
   addName(name) {
