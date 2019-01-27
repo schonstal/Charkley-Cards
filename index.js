c23436780e0d804fa86019eba777d94d9344d70c
Josh Schonstal <josh@schonstal.org>
2019-01-27 11:38:03 -0800

    ✨ Add randomized flavor distribution

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ var port = process.env.PORT || 3000;
 let users = {};
 let channels = {};
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   let _channel = null;
   let _username = null;
 
   socket.on('add_name', function(name) {
-    channels[_channel].users[_username].addName(name);
+    channels[_channel].addName(_username, name);
 
     console.log(`[${new Date()}] ${_username} submitted name ${name} to ${_channel}`);
   });
 
   socket.on('add_flavor', function(flavor) {
-    channels[_channel].users[_username].addFlavor(flavor);
+    channels[_channel].addFlavor(_username, flavor);
 
     console.log(`[${new Date()}] ${_username} submitted flavor ${flavor} to ${_channel}`);
   });
@@ -43,15 +43,25 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(port, function(){
+http.listen(port, function() {
   console.log('listening on *:' + port);
 });
+
+const randomProperty = function(obj) {
+  let keys = Object.keys(obj)
+  let key = keys[keys.length * Math.random() << 0];
+  let property = obj[key];
+
+  return { key, property };
+};
 
 class Channel {
   constructor(name) {
     this.users = {};
     this.currentPhase = 'add-name';
     this.name = name;
+    this.nameSubmissions = {};
+    this.flavorSubmissions = {};
   }
 
   addUser(username, socket) {
@@ -73,8 +83,46 @@ class Channel {
       this.switchPhase('add-flavor');
       setTimeout(() => {
         this.switchPhase('make-cards');
+        this.sendPhrases();
       }, gameTime);
     }, gameTime);
+  }
+
+  sendPhrases() {
+    let availableNames = Object.assign({}, this.nameSubmissions);
+    let availableFlavors = Object.assign({}, this.flavorSubmissions);
+
+    let entries = Object.entries(this.users);
+    for (const [username, user] of entries) {
+      let names = [];
+      let flavors = [];
+
+      for (let i = 0; i < 3; i++) {
+        let name = randomProperty(availableNames);
+        delete availableNames[name.key];
+        names.push({
+          username: name.property,
+          name: name.key
+        });
+
+        let flavor = randomProperty(availableFlavors);
+        delete availableFlavors[flavor.key];
+        flavors.push({
+          username: flavor.property,
+          flavor: flavor.key
+        });
+      }
+
+      user.socket.emit('phrases', { names, flavors });
+    }
+  }
+
+  addName(username, name) {
+    this.nameSubmissions[name] = username;
+  }
+
+  addFlavor(username, flavor) {
+    this.flavorSubmissions[flavor] = username;
   }
 }
 
@@ -83,16 +131,13 @@ class User {
     this.username = username;
 
     this.score = 0;
-    this.nameSubmissions = [];
-    this.flavorSubmissions = [];
     this.socket = socket;
   }
 
-  addName(name) {
-    this.nameSubmissions.push(name);
-  }
-
-  addFlavor(flavor) {
-    this.flavorSubmissions.push(flavor);
+  sendPhrases({ names, flavors }) {
+    this.socket.emit('phrases', {
+      names,
+      flavors
+    });
   }
 }
