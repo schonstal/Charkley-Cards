1c255d08ec5d6b19fa1d49afd19235afda9dbee9
Josh Schonstal <josh@schonstal.org>
2019-01-26 21:45:31 -0800

    ✨ Add primitive channels

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 let users = {};
+let channels = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  let channelName = null;
+
   socket.on('add_name', function({ username, name }) {
     console.log(`${username} submitted name ${name}`);
   });
@@ -18,8 +21,15 @@ io.on('connection', function(socket){
     console.log(`${username} submitted flavor ${flavor}`);
   });
 
-  socket.on('join', function({ username, client_id }) {
-    users[username] = client_id;
+  socket.on('join', function({ username, channel }) {
+    console.log(`got a join for ${channel} by ${username}`);
+
+    if (channels[channel] === undefined) {
+      channels[channel] = new Channel();
+    }
+
+    channelName = channel;
+    channels[channel].addUser(username);
   });
 });
 
@@ -32,3 +42,23 @@ http.listen(port, function(){
     io.emit('chat message', msg);
   });
 */
+
+class Channel {
+  constructor() {
+    this.users = {};
+  }
+
+  addUser(username) {
+    users[username] = new User(username);
+  }
+}
+
+class User {
+  constructor(username) {
+    this.username = username;
+
+    this.score = 0;
+    this.nameSubmissions = [];
+    this.flavorSubmissions = [];
+  }
+}
